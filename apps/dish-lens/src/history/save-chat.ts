51110e71532098f5ec0552ade9e9920cec1d1f96
Davina Leong <leong.shi.yun@gmail.com>
2026-07-31 04:32:49 +0800

    Fixing build errors.

--- apps/dish-lens/src/history/save-chat.ts
+++ apps/dish-lens/src/history/save-chat.ts
@@ -28,7 +28,7 @@ export async function saveChat(params: {
     data: {
       userId: params.userId,
       dishName: params.dishName,
-      messages: params.messages as unknown as Prisma.JsonValue,
+      messages: params.messages as unknown as Prisma.SavedChatCreateInput['messages'],
     },
   });
 
